8fe60c1db6c6dfdf67f93c97341dc07db2996b9f
vaibhavsagar <vaibhavsagar@gmail.com>
2013-12-27 11:49:36 +1100

    It lives!
    Fulfils all requirements now. Most of the core taken from lis.py with
    modifications.

--- toylisp.py
+++ toylisp.py
@@ -58,55 +58,66 @@ def atom(token):
 
 class Env(dict):
     "An environment: a dict of {'var':val} pairs, with an outer Env."
-    def __init__(self, parms=(), args=(), outer=None):
-        self.update(zip(parms,args))
+    def __init__(self, params=(), args=(), outer=None):
+        self.update(zip(params,args))
         self.outer = outer
     def find(self, var):
         "Find the innermost Env where var appears."
-        return self if var in self else self.outer.find(var)
+        return self.get(var, self.outer.find(var) if self.outer else None)
 
 def add_globals(env):
     "Add some Scheme standard procedures to an environment."
-    import math, operator as op
-    env.update(vars(math)) # sin, sqrt, ...
-    env.update(
-     {'+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv, 'not':op.not_,
-      '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq, 
-      'equal?':op.eq, 'eq?':op.is_, 'length':len, 'cons':lambda x,y:[x]+y,
-      'car':lambda x:x[0],'cdr':lambda x:x[1:], 'append':op.add,  
-      'list':lambda *x:list(x), 'list?': lambda x:isinstance(x,list), 
-      'null?':lambda x:x==[], 'symbol?':lambda x: isinstance(x, str)})
+    import operator as op
+    from functools import reduce
+    reducer = lambda o: lambda *args: reduce(o, args)
+    env.update({
+        '+':    reducer(op.add),
+        '-':    reducer(op.sub),
+        '*':    reducer(op.mul),
+        '/':    reducer(op.truediv),
+        'not':  op.not_,
+        '>':    op.gt,
+        '<':    op.lt,
+        '>=':   op.ge,
+        '<=':   op.le,
+        '=':    op.eq,
+        'eq?':  op.eq,
+        'cons': lambda x,y:[x]+y,
+        'car':  lambda x:x[0],
+        'cdr':  lambda x:x[1:],
+        'atom?':lambda x: isinstance(x, str),
+        'else': True
+    })
     return env
 
 global_env = add_globals(Env())
 
-def eval(x, env=global_env):
+def notfound(var):
+    raise NameError("symbol '%s' is not bound to a value" % var)
+
+def eval(e, env=global_env):
     "Evaluate an expression in an environment."
-    if isinstance(x, str):         # variable reference
-        return env.find(x)[x]
-    elif not isinstance(x, list):  # constant literal
-        return x                
-    elif x[0] == 'quote':          # (quote exp)
-        (_, exp) = x
+    if isinstance(e, str):         # variable reference
+        value = env.find(e)
+        return value if value is not None else notfound(e)
+    elif not isinstance(e, list):  # constant literal
+        return e                
+    elif e[0] == 'quote':          # (quote exp)
+        (_, exp) = e
         return exp
-    elif x[0] == 'if':             # (if test conseq alt)
-        (_, test, conseq, alt) = x
-        return eval((conseq if eval(test, env) else alt), env)
-    elif x[0] == 'set!':           # (set! var exp)
-        (_, var, exp) = x
-        env.find(var)[var] = eval(exp, env)
-    elif x[0] == 'define':         # (define var exp)
-        (_, var, exp) = x
+    elif e[0] == 'cond':             # (if test conseq alt)
+        (_, *forms) = e
+        for test, result in forms:
+            if eval(test, env):
+                return eval(result, env)
+    elif e[0] == 'define':         # (define var exp)
+        (_, var, exp) = e
         env[var] = eval(exp, env)
-    elif x[0] == 'lambda':         # (lambda (var*) exp)
-        (_, vars, exp) = x
+    elif e[0] == 'lambda':         # (lambda (var*) exp)
+        (_, vars, exp) = e
         return lambda *args: eval(exp, Env(vars, args, env))
-    elif x[0] == 'begin':          # (begin exp*)
-        for exp in x[1:]:
-            val = eval(exp, env)
-        return val
     else:                          # (proc exp*)
-        exps = [eval(exp, env) for exp in x]
+        exps = [eval(exp, env) for exp in e]
         proc = exps.pop(0)
         return proc(*exps)
 
@@ -114,4 +125,4 @@ def eval(x, env=global_env):
 
 yacc.yacc()
 rep = lambda s: eval(yacc.parse(s))
-if __name__=="__main__": print(eval(yacc.parse("(cons 3 '())")))
+if __name__=="__main__": print(rep("(cons 3 '())"))
