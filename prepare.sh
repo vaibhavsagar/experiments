a943e72e39e493befb845972c5cd31f79f53b98f
vaibhavsagar <vaibhavsagar@gmail.com>
2015-02-15 18:43:42 +1100

    Add -r so that wget works.

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
 sudo apt-get -y install jq
-wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
+wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 sudo dpkg -i pandoc.deb
