9e1341f752eb968bde375f8fa4539b3bf047980a
vaibhavsagar <vaibhavsagar@gmail.com>
2015-02-17 21:59:28 +1100

    No need for alias.

--- prepare.sh
+++ prepare.sh
@@ -5,10 +5,10 @@ wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.ass
 sudo dpkg -i pandoc.deb
 
 # Download an install script for LaTeX
-wget http://mirror.as24220.net/pub/ctan/systems/texlive/tlnet/install-tl-unx.tar.gz
+wget http://mirrors.ibiblio.org/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar xf install-tl-unx.tar.gz
 install-tl-*/install-tl -profile texlive.profile
 sudo apt-get install xzdec -y
 alias tlmgr="/usr/local/texlive/20*/bin/x86_64-linux/tlmgr"
-tlmgr init-usertree
-tlmgr install booktabs preprint
+/usr/local/texlive/20*/bin/x86_64-linux/tlmgr init-usertree
+/usr/local/texlive/20*/bin/x86_64-linux/tlmgr install booktabs preprint
