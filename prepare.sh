f053b9c101d114e502a22609a65b08f6a3dd13c4
vaibhavsagar <vaibhavsagar@gmail.com>
2015-02-17 21:48:20 +1100

    Attempt TeX installation.

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,14 @@
+# Install jq to filter Github release data for Pandoc.
 sudo apt-get -y install jq
+# Get the latest .deb released.
 wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 sudo dpkg -i pandoc.deb
+
+# Download an install script for LaTeX
+wget http://mirror.as24220.net/pub/ctan/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar xf install-tl-unx.tar.gz
+install-tl-*/install-tl -profile texlive.profile
+sudo apt-get install xzdec -y
+alias tlmgr=/usr/local/texlive/20*/bin/x86_64-linux/tlmgr
+tlmgr init-usertree
+tlmgr install booktabs preprint
