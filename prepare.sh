99e75a97b00c18467b164d5d44191bb0957fa7d3
vaibhavsagar <vaibhavsagar@gmail.com>
2015-02-17 22:09:17 +1100

    ls debugging.

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,7 @@ wget http://mirrors.ibiblio.org/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar xf install-tl-unx.tar.gz
 install-tl-*/install-tl -profile texlive.profile
 sudo apt-get install xzdec -y
+ls /usr/local/texlive/2014/bin/x86_64-linux
 alias tlmgr="/usr/local/texlive/2014/bin/x86_64-linux/tlmgr"
 tlmgr init-usertree
 tlmgr install booktabs preprint
