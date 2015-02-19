f07423ca3b68e91975afb6d40c52bb23e596eebb
vaibhavsagar <vaibhavsagar@gmail.com>
2015-02-19 20:21:48 +1100

    See if exporting makes a difference.

--- prepare.sh
+++ prepare.sh
@@ -10,5 +10,6 @@ tar xf install-tl-unx.tar.gz
 sudo install-tl-*/install-tl -profile texlive.profile
 sudo apt-get install xzdec -y
 PATH=/usr/local/texlive/2014/bin/x86_64-linux:$PATH
+export PATH
 sudo env PATH="$PATH" tlmgr init-usertree
 sudo env PATH="$PATH" tlmgr install booktabs preprint
