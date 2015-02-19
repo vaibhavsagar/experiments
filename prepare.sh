b0981502b8bd920b53b35bc81b823b0833c8e765
vaibhavsagar <vaibhavsagar@gmail.com>
2015-02-19 20:35:52 +1100

    Install packages with apt-get.

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 # Install jq to filter Github release data for Pandoc.
-sudo apt-get -y install jq
+sudo apt-get -y install jq xzdec lmodern
 # Get the latest .deb released.
 wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 sudo dpkg -i pandoc.deb
@@ -8,7 +8,6 @@ sudo dpkg -i pandoc.deb
 wget http://mirrors.ibiblio.org/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar xf install-tl-unx.tar.gz
 sudo install-tl-*/install-tl -profile texlive.profile
-sudo apt-get install xzdec -y
 export PATH=/usr/local/texlive/2014/bin/x86_64-linux:$PATH
 sudo env PATH="$PATH" tlmgr init-usertree
-sudo env PATH="$PATH" tlmgr install booktabs preprint lmodern
+sudo env PATH="$PATH" tlmgr install booktabs preprint
