8e75b2ab24c04af2dcc43dc8c2b32da5b478e5be
vaibhavsagar <vaibhavsagar@gmail.com>
2015-03-02 07:19:11 +1100

    More reorganisation.

--- build/build_pdf.sh
+++ build/build_pdf.sh
@@ -2,7 +2,7 @@
 wget http://mirrors.ibiblio.org/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar xf install-tl-unx.tar.gz
 # Install LaTeX
-sudo install-tl-*/install-tl -profile texlive.profile
+sudo install-tl-*/install-tl -profile build/texlive.profile
 export PATH=/usr/local/texlive/2014/bin/x86_64-linux:$PATH
 sudo apt-get -y install xzdec
 sudo env PATH="$PATH" tlmgr init-usertree
