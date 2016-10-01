39a7a6ef751541dcece2a57b76fed61efc02beb3
vaibhavsagar <vaibhavsagar@gmail.com>
2016-10-01 10:43:24 -0400

    Add initial app.py.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    url = request.args.get('url', '')
+    if url:
+        return """<iframe id="main" src="{}" frameborder=0 width=1920 height=1080
+            style="-webkit-transform: rotate(90deg);" target="_parent"></iframe>""".format(url)
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
