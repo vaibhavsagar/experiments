0d0d0501430846b101637955638cc003e1d598ab
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-02 18:04:00 -0400

    Moar style.

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ from os import environ
 app = Flask(__name__)
 port = int(environ.get('PORT', 5000))
 
+content = """
+<body style="width: 1080px; height: 1920px; margin: 0; position: absolute; top: 0; left: 0;">
+    <iframe
+        id="main"
+        src="{}"
+        frameborder=0
+        width=1920
+        height=1080
+        style="transform: rotate(90deg) translate(26em, 26em)" target="_parent">
+    </iframe>
+</body>"""
+
 
 @app.route('/')
 def main():
     url = request.args.get('url', '')
     if url:
-        return """<iframe id="main" src="{}" frameborder=0 width=1920 height=1080
-            style="-webkit-transform: rotate(90deg);" target="_parent"></iframe>""".format(url)
+        return content.format(url)
     return "Hello, World!"
 
 if __name__ == "__main__":
