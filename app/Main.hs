f41de30ad1b6114926b2b204f91849a8118bb03a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-13 09:51:33 -0400

    More than one process.

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,34 @@
 module Main where
 
-import Lib
-import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
+replyBack :: (ProcessId, String) -> Process ()
+replyBack (sender, msg) = send sender msg
+
+logMessage :: String -> Process ()
+logMessage msg = say $ "handling " ++ msg
 
 main :: IO ()
 main = do
     Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
     node    <- newLocalNode t initRemoteTable
-    _       <- runProcess node $ do
-        self  <- getSelfPid
-        send self "hello"
-        hello <- expect :: Process String
-        liftIO $ putStrLn hello
-    return ()
+
+    runProcess node $ do
+        echoPid <- spawnLocal $ forever $ do
+            receiveWait [match logMessage, match replyBack]
+
+        say "send some messages!"
+        send echoPid "hello"
+        self <- getSelfPid
+        send echoPid (self, "hello")
+
+        m <- expectTimeout 1000000
+        case m of
+            Nothing -> die "nothing came back!"
+            Just s  -> say $ "got " ++ s ++ " back!"
+
+    liftIO $ threadDelay 2000000
