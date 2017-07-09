69ad8ffc6faf9cf424e16258eaf1798fb198d697
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-10 00:16:55 +0800

    Add other build inputs for completeness

--- ihaskell-nix/dev-notebook.nix
+++ ihaskell-nix/dev-notebook.nix
@@ -1,5 +1,10 @@
 { pkgs ? import <nixpkgs> {}, packageSrc ? ./. }:
 let
   drv    = pkgs.haskellPackages.callCabal2nix "this" packageSrc {};
-  inputs = with drv; ( propagatedBuildInputs ++ propagatedNativeBuildInputs );
+  inputs = with drv;
+    (  buildInputs
+    ++ nativeBuildInputs
+    ++ propagatedBuildInputs
+    ++ propagatedNativeBuildInputs
+    );
 in import ./release.nix { packages = (_: inputs); }
