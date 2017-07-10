7ba498c385a59a38b25435076932f97a26f63e6a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-10 22:00:03 +0800

    Add current derivation to inputs

--- ihaskell-nix/dev-notebook.nix
+++ ihaskell-nix/dev-notebook.nix
@@ -1,10 +1,11 @@
 { pkgs ? import <nixpkgs> {}, packageSrc ? ./. }:
 let
-  drv    = pkgs.haskellPackages.callCabal2nix "this" packageSrc {};
+  drv    = pkgs.haskell.lib.dontHaddock (pkgs.haskellPackages.callCabal2nix "this" packageSrc {});
   inputs = with drv;
     (  buildInputs
     ++ nativeBuildInputs
     ++ propagatedBuildInputs
     ++ propagatedNativeBuildInputs
+    ++ [ drv ]
     );
 in import ./release.nix { packages = (_: inputs); }
