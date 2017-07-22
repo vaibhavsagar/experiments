8201db2ab8b6cc019f842f3c23528bed9090be8f
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-22 08:19:28 +0800

    Update derivation

--- git-from-scratch/default.nix
+++ git-from-scratch/default.nix
@@ -1,10 +1,12 @@
 import ../ihaskell-nix {
   packages = self: with self; [
     attoparsec
+    byteable
     bytestring
     zlib
     base16-bytestring
     containers
+    SHA
   ];
   systemPackages = pkgs: with pkgs; [
     coreutils
