7731c8c24d49903f40fa88361e0cec7fc6fd9dd9
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-07-25 00:23:43 +0800

    Use IHaskell repo

--- git-from-scratch/default.nix
+++ git-from-scratch/default.nix
@@ -1,4 +1,10 @@
-import ../ihaskell-nix {
+let IHaskell = (import <nixpkgs> {}).fetchFromGitHub {
+    owner  = "gibiansky";
+    repo   = "IHaskell";
+    rev    = "b1a13244228ac209d85c8d113fc73a2e616fd202";
+    sha256 = "12jk24pnhf2cjljp5z3f1ziv8ykaz7p3cnm1jfn6g7lnb33iifbl";
+  };
+in import "${IHaskell}/release.nix" {
   packages = self: with self; [
     SHA
     attoparsec
