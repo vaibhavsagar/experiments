d11183fc0020399dadb5688d4fbdaf7dd8a5333a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-26 10:36:33 +0800

    Add job for building IHaskell with GHC 8.2

--- ihaskell-hydra/release.nix
+++ ihaskell-hydra/release.nix
@@ -1,3 +1,4 @@
 let pkgs = import <nixpkgs> {}; in {
-  build = pkgs.callPackage <ihaskellSrc/release.nix> {};
+  build     = pkgs.callPackage <ihaskellSrc/release.nix> {};
+  build-8_2 = pkgs.callPackage <ihaskellSrc/release-8.2.nix> {};
 }
