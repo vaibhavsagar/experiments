b40d91051c33bb73faf1d3c0b6da396497bb3d6a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-16 00:32:42 +0800

    Don't use 'with'

--- deploy/nix/service.nix
+++ deploy/nix/service.nix
@@ -1,14 +1,12 @@
 { config, lib, pkgs, ... }:
 
-with lib;
-
 let
   cfg = config.services.blank-me-up;
   blank-me-up = pkgs.callPackage ./default.nix { nixpkgs = pkgs; };
 in {
-  options.services.blank-me-up.enable = mkEnableOption "Blank Me Up";
+  options.services.blank-me-up.enable = lib.mkEnableOption "Blank Me Up";
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = [ 3000 ];
 
     systemd.services.blank-me-up = {
