6dbf7bdf6d331527d55395b5c19f9cbf967f6c2e
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-19 02:41:50 +0800

    makehaskell: ghc uses --make by default

--- makehaskell/default.nix
+++ makehaskell/default.nix
@@ -2,7 +2,7 @@
 pkgs.runCommand name { inherit src; } ''
   ${pkgs.coreutils}/bin/mkdir -pv $out/bin
   cd $src
-  ${pkgs.haskellPackages.ghcWithPackages packages}/bin/ghc --make Main.hs \
+  ${pkgs.haskellPackages.ghcWithPackages packages}/bin/ghc Main.hs \
     -outputdir $(${pkgs.coreutils}/bin/mktemp -d) \
     -o $out/bin/${name}
 ''
