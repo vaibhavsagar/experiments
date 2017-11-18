0e2e103682c229e97a5be8ac2f39c6a7cda9f8dc
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-19 02:42:27 +0800

    makehaskell: parametrise entry point

--- makehaskell/default.nix
+++ makehaskell/default.nix
@@ -1,8 +1,8 @@
-{ pkgs ? import <nixpkgs> {}, packages ? (_: []), name ? "main", src }:
+{ main ? "Main.hs", pkgs ? import <nixpkgs> {}, packages ? (_: []), name ? "main", src }:
 pkgs.runCommand name { inherit src; } ''
   ${pkgs.coreutils}/bin/mkdir -pv $out/bin
   cd $src
-  ${pkgs.haskellPackages.ghcWithPackages packages}/bin/ghc Main.hs \
+  ${pkgs.haskellPackages.ghcWithPackages packages}/bin/ghc ${main} \
     -outputdir $(${pkgs.coreutils}/bin/mktemp -d) \
     -o $out/bin/${name}
 ''
