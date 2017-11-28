74908b9f921e8abea833543e520c944ee91e74b7
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-29 00:53:51 +0800

    Simplify version pinning

--- git-from-scratch/default.nix
+++ git-from-scratch/default.nix
@@ -1,9 +1,12 @@
 let
-  fetch    = (import <nixpkgs> {}).fetchFromGitHub;
+  inherit (import <nixpkgs> {}) fetchFromGitHub lib;
+  versions = lib.mapAttrs
+    (_: fetchFromGitHub)
+    (builtins.fromJSON (builtins.readFile ./versions.json));
   # ./updater versions.json ihaskell
-  IHaskell = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).ihaskell;
+  IHaskell = versions.ihaskell;
   # ./updater versions.json nixpkgs nixos-17.09
-  pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
+  pinned   = versions.nixpkgs;
 in import "${IHaskell}/release.nix" {
   nixpkgs = import pinned {};
   packages = self: with self; [
