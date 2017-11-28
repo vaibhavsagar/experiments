1d95c6423ce93e73518faecaab852f14a2fd864d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-29 01:02:54 +0800

    Don't set src in env of runCommand

--- makehaskell/default.nix
+++ makehaskell/default.nix
@@ -1,7 +1,7 @@
 { main ? "Main.hs", pkgs ? import <nixpkgs> {}, packages ? (_: []), name ? "main", src }:
-pkgs.runCommand name { inherit src; } ''
+pkgs.runCommand name {} ''
   ${pkgs.coreutils}/bin/mkdir -pv $out/bin
-  cd $src
+  cd ${src}
   ${pkgs.haskellPackages.ghcWithPackages packages}/bin/ghc ${main} \
     -outputdir $(${pkgs.coreutils}/bin/mktemp -d) \
     -o $out/bin/${name}
