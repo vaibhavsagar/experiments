f1a877743fc3a0d1394ddb03d53b6b705168600b
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-29 01:15:43 +0800

    Allow customising GHC options

--- makehaskell/default.nix
+++ makehaskell/default.nix
@@ -1,8 +1,9 @@
-{ main ? "Main.hs", name ? "main", nixpkgs ? import <nixpkgs> {}, packages ? (_: []), src }:
+{ ghc-options ? "", main ? "Main.hs", name ? "main", nixpkgs ? import <nixpkgs> {}, packages ? (_: []), src }:
 nixpkgs.runCommand name {} ''
   ${nixpkgs.coreutils}/bin/mkdir -pv $out/bin
   cd ${src}
   ${nixpkgs.haskellPackages.ghcWithPackages packages}/bin/ghc ${main} \
+    ${ghc-options} \
     -outputdir $(${nixpkgs.coreutils}/bin/mktemp -d) \
     -o $out/bin/${name}
 ''
