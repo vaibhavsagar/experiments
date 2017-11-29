c72ba9c9b6c15f483855b6e4cc8d5eab786528f5
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-11-29 17:50:09 +0800

    makehaskell: rename main argument to entrypoint

--- makehaskell/default.nix
+++ makehaskell/default.nix
@@ -1,8 +1,8 @@
-{ ghc-options ? "", main ? "Main.hs", name ? "main", nixpkgs ? import <nixpkgs> {}, packages ? (_: []), src }:
+{ entrypoint ? "Main.hs", ghc-options ? "", name ? "main", nixpkgs ? import <nixpkgs> {}, packages ? (_: []), src }:
 nixpkgs.runCommand name {} ''
   ${nixpkgs.coreutils}/bin/mkdir -pv $out/bin
   cd ${src}
-  ${nixpkgs.haskellPackages.ghcWithPackages packages}/bin/ghc ${main} \
+  ${nixpkgs.haskellPackages.ghcWithPackages packages}/bin/ghc ${entrypoint} \
     ${ghc-options} \
     -outputdir $(${nixpkgs.coreutils}/bin/mktemp -d) \
     -o $out/bin/${name}
