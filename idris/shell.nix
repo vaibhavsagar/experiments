ab83453abb6fbfd071e6a141990a8584a7143301
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-01-06 17:24:13 +0800

    idris: pin nixpkgs

--- idris/shell.nix
+++ idris/shell.nix
@@ -1,5 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+  inherit (import <nixpkgs> {}) fetchFromGitHub lib;
+  versions = lib.mapAttrs
+    (_: fetchFromGitHub)
+    (builtins.fromJSON (builtins.readFile ./versions.json));
+  pkgs = import versions.nixpkgs {};
+in
 pkgs.runCommand "dummy" {
   buildInputs = with pkgs; [
     (with idrisPackages; with-packages [ contrib ])
