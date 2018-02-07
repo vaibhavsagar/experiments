29af39176f00847afb4beab682389cff84830d55
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-02-07 16:27:20 +0800

    override-haskell-src-exts: also override hlint

--- override-haskell-src-exts/default.nix
+++ override-haskell-src-exts/default.nix
@@ -13,6 +13,12 @@ let
       rev    = "935f6f0915e89c314b686bdbdc6980c72335ba3c";
       sha256 = "1v3c1bd5q07qncqfbikvs8h3r4dr500blm5xv3b4jqqv69f0iam9";
     }) {};
+    hlint  = self.callCabal2nix "hlint" (nixpkgs.fetchFromGitHub {
+      owner  = "ndmitchell";
+      repo   = "hlint";
+      rev    = "d2609f809e679845b29a9fb5509cad906cf002f5";
+      sha256 = "1270r327k4v0jwnr0m42khjs0gfg987ppr5d5gq9kdrv9vzp07mn";
+    }) {};
   });
 in
   haskellPackages.ghcWithPackages (p: [ p.hlint ])
