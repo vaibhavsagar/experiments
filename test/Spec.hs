6bd8741988fc5dc04c5901b6f8ff0f683ce63b30
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-03-13 23:20:13 +0800

    Add test suite

---
+++ test/Spec.hs
@@ -0,0 +1,55 @@
+import qualified Data.Map.Strict as Map
+import           Data.Map.Strict (Map)
+import           Discount
+import           Test.Hspec
+
+main :: IO ()
+main = do
+    let productDatabase = Map.fromList
+            [ (1, Product "Black Jacobins" 2000)
+            , (2, Product "Freedom Is a Constant Struggle" 1500)
+            ]
+    let discountDatabase = Map.fromList
+            [ (1, Discount "WELCOME" 50  AllProducts)
+            , (2, Discount "JAC75"   75 (ProductList [1]))
+            ]
+    let basicDiscount = computeDiscounts productDatabase discountDatabase
+    let cart = Order [LineItem 1 2, LineItem 2 3] Nothing
+    hspec $ describe "testing discount computation" $ do
+        it "doesn't apply any discount when no code is provided" $ do
+            let expected = Right
+                    [ LineItem (Left (Product "Black Jacobins" 2000)) 2
+                    , LineItem (Left (Product "Freedom Is a Constant Struggle" 1500)) 3
+                    ]
+            let discounted = basicDiscount cart
+            discounted `shouldBe` expected
+            calculateTotal <$> discounted `shouldBe` Right 8500
+        it "applies a 50% off discount to all items" $ do
+            let expected = Right
+                    [ LineItem (Right (DiscountedProduct (Product "Black Jacobins" 2000) 1000)) 2
+                    , LineItem (Right (DiscountedProduct (Product "Freedom Is a Constant Struggle" 1500) 750)) 3
+                    ]
+            let discounted = basicDiscount cart { orderDiscountCode = Just "WELCOME" }
+            discounted `shouldBe` expected
+            calculateTotal <$> discounted `shouldBe` Right 4250
+        it "applies a 75% discount to only one item" $ do
+            let expected = Right
+                    [ LineItem (Right (DiscountedProduct (Product "Black Jacobins" 2000) 500)) 2
+                    , LineItem (Left (Product "Freedom Is a Constant Struggle" 1500)) 3
+                    ]
+            let discounted = basicDiscount cart { orderDiscountCode = Just "JAC75" }
+            discounted `shouldBe` expected
+            calculateTotal <$> discounted `shouldBe` Right 5500
+        it "doesn't apply any discount when an invalid code is provided" $ do
+            let expected = Right
+                    [ LineItem (Left (Product "Black Jacobins" 2000)) 2
+                    , LineItem (Left (Product "Freedom Is a Constant Struggle" 1500)) 3
+                    ]
+            let discounted = basicDiscount cart { orderDiscountCode = Just "FOO" }
+            discounted `shouldBe` expected
+            calculateTotal <$> discounted `shouldBe` Right 8500
+        it "provides a useful error message when a LineItem is not found" $ do
+            let discounted  = basicDiscount $ Order [LineItem 3 1] Nothing
+            discounted `shouldBe` Left "item 3 not found"
+            let discounted' = basicDiscount $ Order [LineItem 3 1] (Just "WELCOME")
+            discounted `shouldBe` Left "item 3 not found"
