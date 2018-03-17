3a9e4c931c55e76cdacac66a437f4478218aabb6
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-03-17 21:39:45 +0800

    stack-env-nix/ihaskell.nix: sync with blog post

--- stack-env-nix/ihaskell.nix
+++ stack-env-nix/ihaskell.nix
@@ -3,18 +3,23 @@ let
 in pkgs.buildFHSUserEnv {
   name = "fhs";
   targetPkgs = pkgs: [
+    pkgs.blas
+    pkgs.cairo.dev
+    pkgs.file
     pkgs.gcc
+    pkgs.glib.dev
     pkgs.gmp
     pkgs.gnumake
     pkgs.haskellPackages.stack
     pkgs.iana-etc
+    pkgs.liblapack
+    pkgs.pango.dev
     pkgs.perl
     pkgs.pkgconfig
     (pkgs.python3.withPackages (ps: [ ps.jupyter ps.notebook ]))
     pkgs.ncurses
-    pkgs.zlib.dev
-    pkgs.zlib.out
     pkgs.zeromq
+    pkgs.zlib.dev
   ];
   profile = ''
     export C_INCLUDE_PATH=/usr/include:$C_INClUDE_PATH
