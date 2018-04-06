9f3bcfd17b4d91cb13dc71e889927bd70dc23f85
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-06 10:56:00 +0800

    idris: use 18.03 for simpler nix-shell

--- idris/shell.nix
+++ idris/shell.nix
@@ -1,38 +1,12 @@
 let
   inherit (import <nixpkgs> {}) fetchFromGitHub lib;
-  overlay = self: super: {
-    idrisPackages = self.callPackage <nixpkgs/pkgs/development/idris-modules> {
-      idris =
-        let
-          inherit (super.haskell) lib;
-          haskellPackages = super.haskellPackages.override {
-            overrides = self: super: {
-              binary = lib.dontCheck self.binary_0_8_5_1;
-              cheapskate = self.cheapskate_0_1_1;
-              idris = lib.overrideCabal (self.callHackage "idris" "1.2.0" {}) (drv: {
-                doCheck = false;
-                librarySystemDepends = (drv.librarySystemDepends or []) ++ [pkgs.gmp];
-                preBuild = ''
-                  export LD_LIBRARY_PATH="$PWD/dist/build:$LD_LIBRARY_PATH"
-                '';
-              });
-              megaparsec = self.callHackage "megaparsec" "6.4.0" {};
-              parser-combinators = self.callHackage "parser-combinators" "0.4.0" {};
-            };
-          };
-        in
-          haskellPackages.idris;
-    };
-  };
   versions = lib.mapAttrs
     (_: fetchFromGitHub)
     (builtins.fromJSON (builtins.readFile ./versions.json));
-  pkgs = import versions.nixpkgs { overlays = [ overlay ]; };
+  nixpkgs = import versions.nixpkgs {};
 in
-pkgs.runCommand "dummy" {
-  buildInputs = with pkgs; with idrisPackages; [
-    (with-packages [ contrib ])
-    gcc
-    gmp
+nixpkgs.mkShell {
+  buildInputs = with nixpkgs.idrisPackages; [
+    (with-packages [ base contrib prelude ])
   ];
-} ""
+}
