d1da0c5eaa766aea66164984523840d8401a131a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-05-09 22:08:46 +0800

    rise: s/writeText/writeTextDir/

--- rise/textfile.nix
+++ rise/textfile.nix
@@ -1,6 +1,6 @@
 let
   pkgs = import <nixpkgs> {};
-in pkgs.writeText "notebook.json" (builtins.toJSON {
+in pkgs.writeTextDir "notebook.json" (builtins.toJSON {
   load_extensions = {
     "rise/main" = true;
   };
