2610ba219681b188d74d8e334536b18eb5f8eb45
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-06-04 00:05:46 +0800

    shell.nix: allow nix-building

--- idris/shell.nix
+++ idris/shell.nix
@@ -4,9 +4,10 @@ let
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
   };
   nixpkgs = import (fetcher (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs) {};
+  idrisWithPackages =
+    with nixpkgs.idrisPackages; (with-packages [ base contrib prelude ]);
+
 in
-nixpkgs.mkShell {
-  buildInputs = with nixpkgs.idrisPackages; [
-    (with-packages [ base contrib prelude ])
-  ];
-}
+  if nixpkgs.lib.inNixShell
+  then nixpkgs.mkShell { buildInputs =  [ idrisWithPackages ]; }
+  else idrisWithPackages
