9ebf5bb0e63c1b2153998e204c3ee62ecacf8c96
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-07-16 00:46:02 +0800

    driftwood: implement Aeson instances for Body

--- driftwood/src/Driftwood.hs
+++ driftwood/src/Driftwood.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Driftwood where
 
 import qualified Data.ByteString as B
 import Data.Aeson
-import Data.Aeson.Types (fieldLabelModifier, Options)
+import Data.Monoid
 import GHC.Generics
 import Data.Char (toLower)
 
@@ -15,10 +16,31 @@ data Message = Message
     , messageBody :: Body
     } deriving (Generic, Show, Eq)
 
+instance FromJSON Message where
+    parseJSON = withObject "Message" $ \v -> Message
+        <$> v .: "src"
+        <*> v .: "dest"
+        <*> v .: "body"
+
+instance ToJSON Message where
+    toJSON (Message src dest body)
+        = object
+        [ "src" .= src
+        , "dest" .= dest
+        , "body" .= body
+        ]
+
+    toEncoding (Message src dest body)
+        = pairs
+        (  "src"  .= src
+        <> "dest" .= dest
+        <> "body" .= body
+        )
+
 data Body
     = RaftInit RaftInitBody
     | RaftInitOk Reply
-    | Error ErrorBody
+    | MError ErrorBody
     | Write WriteBody
     | WriteOk Reply
     | Read Request
@@ -29,11 +51,62 @@ data Body
     | DeleteOk Reply
     deriving (Generic, Show, Eq)
 
+instance FromJSON Body where
+    parseJSON = withObject "Body" $ \v -> v .: "type" >>= \t ->
+        case (t :: String) of
+            "raft_init" -> fmap RaftInit $ RaftInitBody
+                <$> v .: "msg_id"
+                <*> v .: "node_id"
+                <*> v .: "node_ids"
+            "raft_init_ok" -> fmap RaftInitOk $ Reply
+                <$> v .: "in_reply_to"
+            "error" -> fmap MError $ ErrorBody
+                <$> v .:? "code"
+                <*> v .:? "text"
+                <*> v .:  "in_reply_to"
+            "write" -> fmap Write $ WriteBody
+                <$> v .: "msg_id"
+                <*> v .: "key"
+                <*> v .: "value"
+            "write_ok" -> fmap WriteOk $ Reply
+                <$> v .: "in_reply_to"
+            "read" -> fmap Read $ Request
+                <$> v .: "msg_id"
+                <*> v .: "key"
+            "read_ok" -> fmap ReadOk $ ReadReply
+                <$> v .: "in_reply_to"
+                <*> v .: "value"
+            "cas" -> fmap Cas $ CasBody
+                <$> v .: "msg_id"
+                <*> v .: "key"
+                <*> v .: "from"
+                <*> v .: "to"
+            "cas_ok" -> fmap CasOk $ Reply
+                <$> v .: "in_reply_to"
+            "delete" -> fmap Delete $ Request
+                <$> v .: "msg_id"
+                <*> v .: "key"
+            "delete_ok" -> fmap DeleteOk $ Reply
+                <$> v .: "in_reply_to"
+
+instance ToJSON Body where
+    toJSON body = case body of
+        RaftInit (RaftInitBody msg_id node_id node_ids) -> object
+            [ "type"     .= String "raft_init"
+            , "msg_id"   .= msg_id
+            , "node_id"  .= node_id
+            , "node_ids" .= node_ids
+            ]
+        RaftInitOk (Reply in_reply_to) -> object
+            [ "type"        .= String "raft_init_ok"
+            , "in_reply_to" .= in_reply_to
+            ]
+
 data RaftInitBody
     = RaftInitBody
-    { raftInitBodyMsgId   :: Int
-    , raftInitBodyNodeId  :: Int
-    , raftInitBodyNodeIds :: [Int]
+    { raftInitMsgId   :: Int
+    , raftInitNodeId  :: Int
+    , raftInitNodeIds :: [Int]
     } deriving (Generic, Show, Eq)
 
 data Reply = Reply { replyInReplyTo :: Int } deriving (Generic, Show, Eq)
