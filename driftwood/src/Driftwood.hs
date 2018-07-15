dc9658737b6be12c9af50587950af777e103b97d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-07-15 23:27:41 +0800

    driftwood: implement message schema

--- driftwood/src/Driftwood.hs
+++ driftwood/src/Driftwood.hs
@@ -9,23 +9,68 @@ import Data.Aeson.Types (fieldLabelModifier, Options)
 import GHC.Generics
 import Data.Char (toLower)
 
-data Missive = Missive
-    { missiveSrc  :: String
-    , missiveDest :: String
-    , missiveBody :: Object
+data Message = Message
+    { messageSrc  :: String
+    , messageDest :: String
+    , messageBody :: Body
     } deriving (Generic, Show, Eq)
 
-missiveOptions :: Options
-missiveOptions = defaultOptions { fieldLabelModifier = modifyMissiveLabels }
-    where modifyMissiveLabels = map toLower . drop 7
+data Body
+    = RaftInit RaftInitBody
+    | RaftInitOk Reply
+    | Error ErrorBody
+    | Write WriteBody
+    | WriteOk Reply
+    | Read Request
+    | ReadOk ReadReply
+    | Cas CasBody
+    | CasOk Reply
+    | Delete Request
+    | DeleteOk Reply
+    deriving (Generic, Show, Eq)
 
-instance ToJSON Missive where
-    toJSON = genericToJSON missiveOptions
+data RaftInitBody
+    = RaftInitBody
+    { raftInitBodyMsgId   :: Int
+    , raftInitBodyNodeId  :: Int
+    , raftInitBodyNodeIds :: [Int]
+    } deriving (Generic, Show, Eq)
 
-instance FromJSON Missive where
-    parseJSON = genericParseJSON missiveOptions
+data Reply = Reply { replyInReplyTo :: Int } deriving (Generic, Show, Eq)
+
+data Request
+    = Request
+    { requestMsgId :: Int
+    , requestKey   :: String
+    } deriving (Generic, Show, Eq)
+
+data ErrorBody
+    = ErrorBody
+    { errorCode      :: Maybe Int
+    , errorText      :: Maybe String
+    , errorInReplyTo :: Int
+    } deriving (Generic, Show, Eq)
+
+data WriteBody
+    = WriteBody
+    { writeMsgId :: Int
+    , writeKey   :: String
+    , writeValue :: String
+    } deriving (Generic, Show, Eq)
+
+data ReadReply
+    = ReadReply
+    { readInReplyTo :: Int
+    , readValue :: String
+    } deriving (Generic, Show, Eq)
+
+data CasBody
+    = CasBody
+    { casMsgId :: Int
+    , casKey   :: String
+    , casFrom  :: String
+    , casTo    :: String
+    } deriving (Generic, Show, Eq)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
-
-
