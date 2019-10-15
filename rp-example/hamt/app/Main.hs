74dd08486e121effc2a62b67006e1bae2f9e9cab
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-10-15 14:58:10 -0400

    rp-example: no need for OverloadedStrings or TypeApplications

--- rp-example/hamt/app/Main.hs
+++ rp-example/hamt/app/Main.hs
@@ -1,6 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo       #-}
-{-# LANGUAGE TypeApplications  #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import Reflex
@@ -20,11 +18,11 @@ import Language.Javascript.JSaddle
 
 data Op = InsertTree | DeleteTree
 
-main = mainWidgetWithHead widgetHead $ el "div" $ do
-  key <- valueInput "key"
-  val <- valueInput "value"
-  b <- button "insert"
-  d <- button "delete"
+main = mainWidgetWithHead widgetHead $ el (pack "div") $ do
+  key <- valueInput (pack "key")
+  val <- valueInput (pack "value")
+  b <- button (pack "insert")
+  d <- button (pack "delete")
   let events = leftmost [InsertTree <$ b, DeleteTree <$ d]
   let values = zipDynWith (,) key val
   tree <- foldDyn
@@ -34,9 +32,9 @@ main = mainWidgetWithHead widgetHead $ el "div" $ do
     None
     (attachPromptlyDyn values events)
   let hamtDot = fmap (pack . dotFromHAMT) tree
-  text " = "
-  graphVizDiv <- _element_raw . fst <$> el' "div" blank
-  el "pre" $ dynText hamtDot
+  text (pack " = ")
+  graphVizDiv <- _element_raw . fst <$> el' (pack "div") blank
+  el (pack "pre") $ dynText hamtDot
   performEvent_ $ ffor (updated hamtDot) $ \dotText ->
     liftJSM . join $ viz <$> toJSVal graphVizDiv <*> toJSVal dotText
   where
@@ -44,31 +42,32 @@ main = mainWidgetWithHead widgetHead $ el "div" $ do
     widgetHead = do
       script "https://cdn.jsdelivr.net/npm/viz.js@2.1.2/viz.min.js"
       script "https://cdn.jsdelivr.net/npm/viz.js@2.1.2/full.render.min.js"
-    script src = elAttr "script" ("type" =: "text/javascript" <> "src" =: src) blank
+    script src = elAttr (pack "script") (pack "type" =: pack "text/javascript" <> pack "src" =: pack src) blank
 
 viz :: JSVal -> JSVal -> JSM ()
 viz graphVizDiv dot = do
-  viz <- new (jsg @Text "Viz") ()
-  render <- viz ^. js1 @Text "renderSVGElement" dot
-  andThen <- render ^. js1 @Text "then" (fun $ \_ _ [element] -> do
-    outerHTML <- element ! ("outerHTML" :: Text)
-    graphVizDiv ^. jss @Text "innerHTML" outerHTML)
-  void $ andThen ^. js1 @Text "catch" (fun $ \_ _ [err] -> void $
-    jsg @Text "console" >>= \console -> console ^. js1 @Text "log" err)
+  viz <- new (jsg "Viz") ()
+  render <- viz ^. js1 "renderSVGElement" dot
+  andThen <- render ^. js1 "then" (fun $ \_ _ [element] -> do
+    outerHTML <- element ! "outerHTML"
+    graphVizDiv ^. jss "innerHTML" outerHTML)
+  void $ andThen ^. js1 "catch" (fun $ \_ _ [err] -> void $
+    jsg "console" >>= \console -> console ^. js1 "log" err)
 
 valueInput :: (DomBuilder t m, MonadFix m) => Text -> m (Dynamic t String)
 valueInput placeholder = do
-  let initAttrs = ("type" =: "string") <> (style False) <> ("placeholder" =: placeholder)
-      color error = if error then "red" else "green"
-      style error = "style" =: ("border-color: " <> color error)
+  let initAttrs = (attr "type" =: pack "string") <> (style False) <> (attr "placeholder" =: placeholder)
+      color error = pack $ if error then "red" else "green"
+      style error = attr "style" =: (pack "border-color: " <> color error)
       styleChange :: Maybe Double -> Map AttributeName (Maybe Text)
       styleChange result = case result of
         (Just _) -> fmap Just (style False)
         (Nothing) -> fmap Just (style True)
+      attr = AttributeName Nothing . pack
 
   rec
     n <- inputElement $ def
-      & inputElementConfig_initialValue .~ ""
+      & inputElementConfig_initialValue .~ pack ""
       & inputElementConfig_elementConfig . elementConfig_initialAttributes .~ initAttrs
       -- & inputElementConfig_elementConfig . elementConfig_modifyAttributes .~ modAttrEv
     let result = fmap unpack $ _inputElement_value n
