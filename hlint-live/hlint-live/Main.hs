68a79d1485a172c0e6ebbcaf49fbe9c30cefb556
Vaibhav Sagar <vaibhavsagar@gmail.com>
2020-05-24 22:32:58 +0800

    hlint-live: handle exceptions from invalid code

--- hlint-live/hlint-live/Main.hs
+++ hlint-live/hlint-live/Main.hs
@@ -1,24 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+
 import Reflex.Dom
 import Language.Haskell.HLint
 import qualified Data.Text as T
 import Control.Monad.IO.Class (liftIO)
+import Control.Exception
 import System.IO.Unsafe (unsafePerformIO)
 import Data.FileEmbed
 import HLintPath
 
 main = mainWidget $ el "div" $ do
-  t  <- _textAreaElement_value <$> textAreaElement def
-  ideas <- performEvent $ ffor (updated t) $ \text -> liftIO $
-    lint (T.unpack text)
-  ideasDyn <- holdDyn [] ideas
-  el "div" $
-    dynText $ T.pack . show <$> ideasDyn
+    t  <- _textAreaElement_value <$> textAreaElement def
+    ideas <- performEvent $ ffor (updated t) $ \text -> liftIO $ do
+        lint (T.unpack text)
+    ideasDyn <- holdDyn Nothing ideas
+    el "div" $
+        dynText $ T.pack . displayLint <$> ideasDyn
+
+displayLint :: Maybe [Idea] -> String
+displayLint Nothing = "Initialising..."
+displayLint (Just ideas) = show ideas
 
 -- Initialise hlint settings
 {-# NOINLINE settings #-}
+settings :: (ParseFlags, [Classify], Hint)
 settings = unsafePerformIO autoSettings'
 
 hlintYamlContents :: String
@@ -32,15 +40,20 @@ autoSettings' = do
     (fixities, classify, hints) <- findSettings (readSettingsFile' Nothing) Nothing
     pure (parseFlagsAddFixities fixities defaultParseFlags, classify, hints)
 
-lint :: String -> IO [Idea]
+lint :: String -> IO (Maybe [Idea])
 lint code = do
-  let (flags, classify, hint) = settings
+    let (flags, classify, hint) = settings
 
-  parsed <- parseModuleEx flags "-" (Just code)
+    parsed <- parseModuleEx flags "-" (Just code)
 
-  -- create 'ideas'
-  let ideas = case parsed of
-        Left _ -> []
-        Right mods -> applyHints classify hint [mods]
+    -- create 'ideas'
+    ideas <- case parsed of
+        Left _ -> pure (Just [])
+        Right mods -> do
+            ideasEither <- try @SomeException . evaluate $
+                applyHints classify hint [mods]
+            case ideasEither of
+                Left _ -> pure (Just [])
+                Right ideas -> pure (Just ideas)
 
-  return ideas
+    pure ideas
