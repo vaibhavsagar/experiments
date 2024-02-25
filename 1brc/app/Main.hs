9f738a0de0cfdeee5b546cf2589fc5d8b9da290f
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-02-25 14:17:30 +1100

    1brc: first draft

--- 1brc/app/Main.hs
+++ 1brc/app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import Control.Monad (when)
+import System.Environment
+
+import Debug.Trace
+
+import OneBRC
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    when (length args < 1) $ error "no filename provided"
+    let filename = head args
+    contents <- readFile filename
+    traceIO $ "read contents"
+    let processed = processAll contents
+    traceIO $ "processed contents"
+    let calculated = calculateAll processed
+    traceIO $ "calculated contents"
+    let formatted = formatMap calculated
+    traceIO $ "formatted contents"
+    let output = formatOutput formatted
+    traceIO $ "formatted output"
+    putStrLn output
--- 1brc/src/OneBRC.hs
+++ 1brc/src/OneBRC.hs
@@ -1,1 +1,49 @@
 module OneBRC where
+
+import qualified Data.Map.Strict as Map
+import Data.List
+import Text.Printf
+
+data Measure = Measure
+    { measureMin :: Double
+    , measureMax :: Double
+    , measureAvg :: Double
+    } deriving (Eq, Show)
+
+formatMeasure :: Measure -> String
+formatMeasure m = printf "%.1f/%.1f/%.1f" (measureMin m) (measureAvg m) (measureMax m)
+
+parseLine :: String -> (String, Double)
+parseLine line = let
+    (name, rest) = break (==';') line
+    measurement = read (tail rest)
+    in (name, measurement)
+
+insertLine :: Map.Map String [Double] -> String -> Map.Map String [Double]
+insertLine measurements line = let
+    (name, measurement) = parseLine line
+    measurements' = Map.insertWith (<>) name [measurement] measurements
+    in measurements'
+
+calculateMeasure :: [Double] -> Measure
+calculateMeasure ds = Measure
+    { measureMin = minimum ds
+    , measureMax = maximum ds
+    , measureAvg = sum ds / genericLength ds
+    }
+
+processAll :: String -> Map.Map String [Double]
+processAll fileContents = foldl' insertLine Map.empty (lines fileContents)
+
+calculateAll :: Map.Map String [Double] -> Map.Map String Measure
+calculateAll = Map.map calculateMeasure
+
+formatMap :: Map.Map String Measure -> Map.Map String String
+formatMap = Map.map formatMeasure
+
+formatOutput :: Map.Map String String -> String
+formatOutput formattedMap = let
+    pairs = Map.toAscList formattedMap
+    equals = map (\(k,v) -> k ++ "=" ++ v) pairs
+    commas = intercalate ", " equals
+    in "{" ++ commas ++ "}"
